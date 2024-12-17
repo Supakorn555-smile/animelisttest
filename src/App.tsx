103f4104e183fbb2a729c1f1210e8233cff787c3
unknown <supakornwongsawat0@gmail.com>
2024-12-17 19:59:08 +0700

    reviserouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,39 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home';
 import DetailPage from './pages/detail';
-import Footer from './components/Footer/Footer';
 import Season from './pages/season';
 import { Favorite } from './pages/favorite';
-import Navbar from './components/Navbar/Navbar';
+import Root from './pages/Root';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/season",
-      element: <Season />,
-    },
-    {
-      path: "/detail/:mal_id",
-      element: <DetailPage />,
-    },
-    {
-      path: "/favorite",
-      element: <Favorite />,
-    },
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/season",
+          element: <Season />,
+        },
+        {
+          path: "/detail/:mal_id",
+          element: <DetailPage />,
+        },
+        {
+          path: "/favorite",
+          element: <Favorite />,
+        },
+      ]
+    }
   ], { basename: import.meta.env.DEV ? '/' : '/animelisttest/' });
 
   return (
     <div className='bg-[url("/images/Webbackground.jpg")] min-h-screen bg-cover bg-fixed'>
-
-      <Navbar />
-
-
       <RouterProvider router={router} />
-
-
-      <Footer />
     </div>
   );
 }
